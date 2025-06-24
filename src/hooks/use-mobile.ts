10712d380fdf86a2c09eb2f995b1edcd5472ae39
chhavi167 <chhavirathi246@gmail.com>
2025-06-25 01:40:51 +0530

    use-mobile.ts

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,7 +1,31 @@
 
-const useIsMobile = () => {
-    return window.innerWidth < 768;
-  };
-  
-  export default useIsMobile;
+import { useEffect, useState } from 'react';
+
+export default function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 576);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 576) {
+        if (!isMobile) {
+          setIsMobile(true);
+        }
+      } else {
+        if (isMobile) {
+          setIsMobile(false);
+        }
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
+  return isMobile;
+}
   
